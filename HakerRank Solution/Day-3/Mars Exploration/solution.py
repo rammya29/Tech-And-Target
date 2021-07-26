6a8092fc1022a621dc505fe48dd844a4b8193cc1
Rammya Dharshini K <70591317+rammya29@users.noreply.github.com>
2021-07-26 11:13:16 +0530

    Create solution.py

---
+++ HakerRank Solution/Day-3/Mars Exploration/solution.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    s = s.strip()
+    return(len(s)-s[::3].count('S')-s[1::3].count('O')-s[2::3].count('S'))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
